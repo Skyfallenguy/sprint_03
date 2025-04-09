df5b9517d8a6c0e0bb40f495e70326a7e69d3d56
skyfallen- <kaernick@yandex.ru>
2025-04-10 00:35:15 +0300

    Добавлен счетчик по нажатию на кнопку

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var userCounterLabel: UILabel!
+    @IBOutlet weak var counterButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        userCounterLabel.text = "Значение счетчика: 0"
+        counterButton.setTitle("Увеличить на 1", for: .normal)
     }
+    var counter: Int = 0
 
-
+    
+    @IBAction func ButtonDidTap(_ sender: Any) {
+        counter += 1
+        userCounterLabel.text = "Значение счетчика: \(counter)"
+    }
 }
 
