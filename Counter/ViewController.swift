08883d4c2abbc8cde97d2c264290d6a0af390629
skyfallen- <kaernick@yandex.ru>
2025-04-10 00:10:02 +0300

    Создан проект

---
+++ Counter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Counter
+//
+//  Created by Nikita Kartavykh on 10/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
